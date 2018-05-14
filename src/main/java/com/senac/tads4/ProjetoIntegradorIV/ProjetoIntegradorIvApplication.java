0c91497f4dbfda6128a815c206ebd8b3c96c099c
Pedro Lucas <Pedro Lucas@192.168.0.101>
2018-05-14 02:26:07 -0300

    Configurado Thymeleaf e função de cadastramento no banco de dados. Cadastrar esta funcionando.

--- src/main/java/com/senac/tads4/ProjetoIntegradorIV/ProjetoIntegradorIvApplication.java
+++ src/main/java/com/senac/tads4/ProjetoIntegradorIV/ProjetoIntegradorIvApplication.java
@@ -1,5 +1,6 @@
 package com.senac.tads4.ProjetoIntegradorIV;
 
+import com.senac.tads4.ProjetoIntegradorIV.model.Cliente;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +9,6 @@ public class ProjetoIntegradorIvApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoIntegradorIvApplication.class, args);
+                
 	}
 }
--- src/main/java/com/senac/tads4/ProjetoIntegradorIV/controller/IndexController.java
+++ src/main/java/com/senac/tads4/ProjetoIntegradorIV/controller/IndexController.java
@@ -5,11 +5,19 @@
  */
 package com.senac.tads4.ProjetoIntegradorIV.controller;
 
+import com.senac.tads4.ProjetoIntegradorIV.dao.ClienteServiceJpaImpl;
+import com.senac.tads4.ProjetoIntegradorIV.model.Cliente;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import com.senac.tads4.ProjetoIntegradorIV.service.ICliente;
 
 /**
  *
@@ -21,6 +29,10 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/musicstore")
 public class IndexController {
     
+  @Autowired
+  public ICliente servicoCliente;
+    
+    
   @GetMapping("/home")
   public ModelAndView executar1() {
     return new ModelAndView("index");
@@ -31,10 +43,43 @@ public class IndexController {
     return new ModelAndView("cadastro_produto");
   }
   
+  //-----------COMEÇO DE FUNÇÕES CONTROLLER DO CLIENTE -------------------------
   @GetMapping("/cadastrousuario")
   public ModelAndView cadastrousuario() {
-    return new ModelAndView("cadastro_usuario");
+    return new ModelAndView("cadastro_usuario").addObject("cliente", new Cliente());
   }
+  
+  @PostMapping("/salvar")
+  public ModelAndView salvar(@ModelAttribute("cliente") @Valid Cliente cliente ,
+          BindingResult bindingResult,
+          RedirectAttributes redirectAttributes) {
+    // @ModelAttribute - indica para o Spring que as informacoes preenchidas
+    // no form HTML serao populadas no objeto produto
+    // @Valid - indica que o objeto será validado de acordo com as configurações
+    // feitas na classe Produto
+   
+    
+    //if (bindingResult.hasErrors()) {
+      //return new ModelAndView("/produto/formulario");
+    //}     
+   
+    if (cliente.getCodigo() == null) {
+      servicoCliente.incluir(cliente);
+    } else {
+    servicoCliente.alterar(cliente);
+    }
+   
+    redirectAttributes.addFlashAttribute("clienteCadastrar", cliente);
+    return new ModelAndView("redirect:/musicstore/home");
+  }
+ @GetMapping("/resultado")
+  public ModelAndView mostrarResultado() {
+    return new ModelAndView("/produto/index");
+  }
+  
+  
+  
+  
   
   @GetMapping("/faleconosco")
   public ModelAndView faleconosco() {
